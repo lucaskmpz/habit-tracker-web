f7f1aa86f3f64bf323451c3a0a5fd0aa32250e5e
Lucas <lucasclmaia0@gmail.com>
2023-09-29 20:56:50 -0300

    creating summary table

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,10 @@ export default {
       colors: {
         background: "#09090A",
       },
+
+      gridTemplateRows: {
+        7: "repeat(7, minmax(0, 1fr)",
+      },
     },
   },
   plugins: [],
