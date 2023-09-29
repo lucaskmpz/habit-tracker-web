c55e4407ff14ae7e6e1f25404bd0683922642ad8
Lucas <lucasclmaia0@gmail.com>
2023-09-29 20:49:56 -0300

    bug fixing on react component header

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import "./styles/global.css"
-import { Plus } from "phosphor-react"
+import { Header } from "./components/Header"
 
 // import { Habit } from "./components/Habit"
 
@@ -8,9 +8,7 @@ export function App() {
   return (
     <div className="w-screen h-screen flex justify-center items-center">
       <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
-
-
-
+        <Header />
       </div>
     </div>
   )
