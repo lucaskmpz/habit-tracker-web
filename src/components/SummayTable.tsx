41999d63e0d4b4048e687381e8bcb8d43a1a6cc4
Lucas <lucasclmaia0@gmail.com>
2023-09-29 21:12:20 -0300

    splitting box creation content into new react component HabitDay

--- src/components/SummayTable.tsx
+++ src/components/SummayTable.tsx
@@ -1,3 +1,5 @@
+import { HabitDay } from "./HabitDay"
+
 const weekDays = [
     "D",
     "S",
@@ -21,8 +23,8 @@ export function SummaryTable() {
                 })}
             </div>
 
-            <div className="grid grid-rows-7 grid-flow-row gap-3">
-                <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg"></div>
+            <div className="grid grid-rows-7 grid-flow-col gap-3">
+                <HabitDay />
             </div>
         </div>
     )
