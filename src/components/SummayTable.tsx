091659e24f6a336033b8a7e48172ac88280fbf9c
Lucas <lucasclmaia0@gmail.com>
2023-09-29 21:25:42 -0300

    fixing automation from box creation

--- src/components/SummayTable.tsx
+++ src/components/SummayTable.tsx
@@ -1,3 +1,4 @@
+import { generateDatesFromYearBeginning } from "../utils/generate-dates-from-year-beginning"
 import { HabitDay } from "./HabitDay"
 
 const weekDays = [
@@ -9,6 +10,8 @@ const weekDays = [
     "S",
     "S",
 ]
+
+const summaryDates = generateDatesFromYearBeginning()
 
 export function SummaryTable() {
     return (
@@ -24,7 +27,9 @@ export function SummaryTable() {
             </div>
 
             <div className="grid grid-rows-7 grid-flow-col gap-3">
-                <HabitDay />
+                {summaryDates.map(date => {
+                    return <HabitDay key={date.toString} />
+                })}
             </div>
         </div>
     )
